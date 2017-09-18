066da85d79d10c759cfe764457e541256533a128
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2017-09-18 14:18:34 +0200

    More comments in demo

--- theories/Demo.v
+++ theories/Demo.v
@@ -3,24 +3,33 @@ Require Import Smpl.
 (** Smpl provides extendable tactics that are applied in order until the first succeeds. *)
 Smpl Create len.
 
+(** Example: We define a tactic that will simplify expressions involving List.length.
+    Note that repeat will succeed if [smpl len] fails, hence we use the folloing definition: *)
 Ltac len_simpl := smpl len; repeat (smpl len).
 
-(*
-Smpl warns you when you used undeclared databases in tactics.
-Ltac test := smpl foo.
-*)
+(**
+Smpl fails at parsing if an undeclared databases is used in tactics.
+This is possible, because a tactic can only use a database if it has been
+created with the command [Smpl Create] before.
+[smpl foo]
+ *)
+
+Goal False.
+  (** Application of an empty database alyways fails. *)
+  Fail smpl len.
+Abort.
 
 Require Import List.
 
 Notation "f ⊝ L" := (List.map f L) (at level 50, L at level 50, left associativity).
 
-(* This adds the tactic at priority 100. *)
 Ltac len_simpl_app :=
   match goal with
   | [ |- context [ length (?L ++ ?L') ] ] => rewrite (@app_length _ L L')
   | [ H : context [ length (?L ++ ?L') ] |- _ ] => rewrite (@app_length _ L L') in H
   end.
 
+(* This adds the tactic at priority 100. *)
 Smpl Add len_simpl_app : len.
 
 Smpl Print len.
@@ -43,39 +52,74 @@ Hint Extern 0 => len_simpl.
 
 Goal (forall X (f:X->X) L L', length (f ⊝ (L ++ L')) = length (f ⊝ L ++ f ⊝ L')).
   eauto.
+  Restart.
+  intros. len_simpl. reflexivity.
 Qed.
 
-(* smpl works across modules, like eauto databases.
+(** smpl works across modules, like eauto databases.
    This means the tactic [len_simpl] can be modularily
    extended with additional simplification tactics. *)
 
-(** Each smpl database can be configured to require progress
-of the tactic. The default is to not require progress. *)
+(* Like Ltac, smpl does not work across sections: *)
+Section Foo.
+  Ltac bar := idtac.
+  Goal False.
+    bar.
+  Abort.
+  (* This database won't be available outside of the section *)
+  Smpl Create foo.
 
-Smpl Create noprogress.
+  (* This addition won't survive the section "End". *)
+  Smpl Add 101 bar : len.
+  Smpl Print len.
+End Foo.
 
-Smpl Print noprogress.
+Goal False.
+  (* Ltac definition no available  *)
+  Fail bar.
+Abort.
+
+(* Note that bar is not in the tactics anymore *)
+Smpl Print len.
+
+
+(** Each smpl database can be configured to wrap added tactics with
+a call to the tactic [progress]. The default is to not wrap with [progress]. *)
+
+Smpl Create example1.
+Smpl Create example2 [noprogress].
+Smpl Create example3 [progress].
+
+Smpl Print example1.
+Smpl Print example2.
+Smpl Print example3.
 
 (** The default can be overwritten for individual tactics
 with the options [progress] and [noprogress] *)
 
-Smpl Add [progress] idtac : noprogress.
+Smpl Add [progress] idtac : example1.
+Smpl Add idtac : example2.
+Smpl Add idtac : example3.
 
-Smpl Print noprogress.
+Smpl Print example1.
+Smpl Print example2.
+Smpl Print example3.
 
 Goal True.
 Proof.
-  Smpl Print noprogress.
-	Fail smpl noprogress.
-	first [ idtac ].
-	Fail progress first [ idtac ].
-	Smpl Add (assert True by auto) : noprogress.
-	smpl noprogress.
+  Smpl Print example1.
+  (** The following tactic application fails, because no progress is made *)
+	Fail smpl example1.
+  (** The following tactic application succeeds, because progress is not required *)
+  smpl example2.
+  (* Note that creating new hypotheses constitutes progress: *)
+	Smpl Add (assert True by auto) : example1.
+	smpl example1.
 	assumption.
 Qed.
 
 
-(** Smpl can also create tactics that take arguments. *)
+(** Smpl can also works with tactics that take arguments. *)
 
 Smpl Create smpl_with_arg.
 
